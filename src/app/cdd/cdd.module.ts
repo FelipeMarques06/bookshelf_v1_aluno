f6129e98275ef9e0f8b67d48187d5ded18e5579e
Felipe <felipemph2@gmail.com>
2022-01-11 14:07:17 -0300

    Adição de serviço de gênero e end point da API

--- src/app/cdd/cdd.module.ts
+++ src/app/cdd/cdd.module.ts
@@ -1,14 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatTableModule } from '@angular/material/table';
-import { MatToolbarModule } from '@angular/material/toolbar';
 
+import { AppMaterialModule } from './../compartilhado/app-material.module';
 import { CddRoutingModule } from './cdd-routing.module';
 import { ClassesComponent } from './classes/classes.component';
-
 
 @NgModule({
   declarations: [
@@ -17,11 +12,7 @@ import { ClassesComponent } from './classes/classes.component';
   imports: [
     CommonModule,
     CddRoutingModule,
-    MatTableModule,
-    MatCardModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule
+    AppMaterialModule
   ]
 })
 export class CddModule { }
