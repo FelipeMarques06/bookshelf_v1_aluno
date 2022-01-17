926a6f33270768ae4345e4ae5e5799bff97cc07c
Felipe <felipemph2@gmail.com>
2022-01-17 09:10:34 -0300

    Construção de dialogos e configuração dos pipes.

--- src/app/cdd/service/generos.service.ts
+++ src/app/cdd/service/generos.service.ts
@@ -1,6 +1,7 @@
 import { Generos } from './../modelos/generos';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { delay, first, tap } from 'rxjs';
 
 
 @Injectable({
@@ -13,6 +14,11 @@ export class GenerosService {
   constructor(private clienteDados: HttpClient) { }
 
   listagemGeneros(){
-    return this.clienteDados.get<Generos[]>(this.urlAPI);
+    return this.clienteDados.get<Generos[]>(this.urlAPI)
+    .pipe(
+      delay(500),
+      first(),
+      tap(apiGeneros => console.log(apiGeneros))
+    )
   }
 }
