85116c0dbbc8cade1511611c7b5f784b7a50ab8f
Felipe <felipemph2@gmail.com>
2022-01-20 09:14:15 -0300

    Adição do dialogo de login na nav

--- src/app/navegacao/navegacao.component.ts
+++ src/app/navegacao/navegacao.component.ts
@@ -1,9 +1,12 @@
-import { NavegacaoService } from './../servicosInterface/navegacao.service';
-import { MenuNavegador } from './../modelosInterface/menuNavegador';
-import { Component } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
-import { Observable, catchError, of } from 'rxjs';
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
+
+import { AppLoginComponent } from './../app-login/app-login.component';
+import { MenuNavegador } from './../modelosInterface/menuNavegador';
+import { NavegacaoService } from './../servicosInterface/navegacao.service';
 
 @Component({
   selector: 'app-navegacao',
@@ -11,33 +14,35 @@ import { map, shareReplay } from 'rxjs/operators';
   styleUrls: ['./navegacao.component.scss']
 })
 export class NavegacaoComponent {
-  //Itens do Menu Principal
-  tituloNav = 'BookShelf V1';
-
-  //Itens de ícones e imagens de navegação
-  iconeGeral = '../../assets/imagens/ShelfBook.png';
-  lIcone = 80;
-  aIcone = 80;
-
-  //Controle das rotas do menu
-  itensMenu$: Observable<MenuNavegador[]>;
-
+  //Itens co menu principal.
+  tituloNav='BookShelf v1';
+  //Itens de icones e imagens de navegação.
+  iconeGeral='../../assets/imagens/ShelfBook.png';
+  lIcone=80;
+  aIcone=80;
+  //Controle das rotas do menu.
+  itensMenu$: Observable<MenuNavegador[]>
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches),
       shareReplay()
     );
-
   constructor(
     private breakpointObserver: BreakpointObserver,
-    private navegadorService: NavegacaoService) {
+    private telaLogin: MatDialog,
+    private navegadorService: NavegacaoService
+    ) {
       this.itensMenu$ = navegadorService.listagemMenu()
       .pipe(
         catchError(error =>{
-          return of([]);
+          return of([])
         })
       )
-
     }
 
+    abrirLogin(erroMsg: string){
+      this.telaLogin.open(AppLoginComponent,{
+        data: erroMsg
+      })
+    }
 }
